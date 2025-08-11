69bc891406b9f5c45ed3c1d4b3634ba4d00051e9
aditi <aditideshpande2908@gmail.com>
2025-08-11 15:44:30 +0530

    currency exercise to showcase how useEffect is used

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Anecdote from './exercises/Anectode'
 import Course from './exercises/Course'
 import Phonebook from './exercises/Phonebook.jsx'
+import Currency from './exercises/Currency.jsx'
 // import { courses } from './data.js'
 
 const App = () => {
@@ -8,7 +9,8 @@ const App = () => {
 		<div>
 			{/* <Anecdote />
 			<Course courses={courses} /> */}
-			<Phonebook />
+			{/* <Phonebook /> */}
+			<Currency />
 		</div>
 	)
 }
---
+++ src/exercises/Currency.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Currency = () => {
+	const [value, setValue] = useState('')
+	const [rates, setRates] = useState('')
+	const [currency, setCurrency] = useState(null)
+
+	useEffect(() => {
+		console.log('effect run, currency is now', currency)
+		// skip if currency is not defined
+		if (currency) {
+			console.log('fetching exchange rates...')
+			axios
+				.get(`https://open.er-api.com/v6/latest/${currency}`)
+				.then((response) => {
+					setRates(response.data.rates)
+				})
+		}
+	}, [currency])
+
+	const onSearch = (e) => {
+		e.preventDefault()
+		setCurrency(value)
+	}
+	return (
+		<div>
+			<form onSubmit={onSearch}>
+				Currency:{' '}
+				<input
+					type='text'
+					value={value}
+					onChange={(e) => setValue(e.target.value)}
+				/>
+				<button type='submit'>exchange rate</button>
+			</form>
+			<pre>{JSON.stringify(rates, null, 2)}</pre>
+		</div>
+	)
+}
+
+export default Currency
